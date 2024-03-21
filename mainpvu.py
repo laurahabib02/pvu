ea357f4a26a042e5a230fd93f23aae22aa19d752
laurahabib02 <laurahabib02@gmail.com>
2024-03-21 14:00:44 +0100

    hh

--- mainpvu.py
+++ mainpvu.py
@@ -8,5 +8,29 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
+# Definiera alla motorer och sensorer
+ev3 = EV3Brick()
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+gripper_motor = Motor(Port.A)
+touch_sensor = TouchSensor(Port.S1)
+color_sensort = ColorSensor(Port.S2)
+
 # Steg 1: öppna och stänga klon, requirement 1
 
+def open_grip():
+    grip_motor.run_to_rel_pos(position_sp=-500, speed_sp=250)  # Justera dessa värden
+    grip_motor.wait_while('running')
+    time.sleep(0.5)
+
+# Funktion för att stänga greppet
+def close_grip():
+    grip_motor.run_forever(speed_sp=100)  # Justera hastigheten efter behov
+    while not touch_sensor.is_pressed:  # Vänta tills touch sensorn är aktiverad
+        pass
+    grip_motor.stop()
+    Sound.beep()  # Bekräftelse på att objektet har greppats
+
+# Huvudprogram
+open_grip()
+close_grip()
